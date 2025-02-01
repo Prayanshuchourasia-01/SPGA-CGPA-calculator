5da90b82402c09024f4bd5e687e4abd22a017b9a
PrayanshuChourasia <prayanshuchourasia440@gmail.com>
2025-02-01 09:36:00 +0530

    UI desgined for SGPA

---
+++ script.js
@@ -0,0 +1,13 @@
+const Total_ThMarks = document.getElementById('ThSub1').value + document.getElementById('ThSub2').value + document.getElementById('ThSub3').value + document.getElementById('ThSub4').value + document.getElementById('ThSub5').value  ;
+
+const Total_ThCredit = document.getElementById('ThCr1').value + document.getElementById('ThCr2').value + document.getElementById('ThCr3').value + document.getElementById('ThCr4').value + document.getElementById('ThCr5').value  ;
+
+const Total_PrMarks = document.getElementById('PrSub1').value + document.getElementById('PrSub2').value + document.getElementById('PrSub3').value + document.getElementById('PrSub4').value + document.getElementById('PrSub5').value  ;
+
+const Total_PrCredit = document.getElementById('PrCr1').value + document.getElementById('PrCr2').value + document.getElementById('PrCr3').value + document.getElementById('PrCr4').value + document.getElementById('PrCr5').value  ;
+
+function calculate(){
+    alert(Total_ThCredit);
+}
+
+// Demo of the Control flow of the Calculation 
