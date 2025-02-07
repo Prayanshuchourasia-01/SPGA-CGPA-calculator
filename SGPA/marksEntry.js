62951dea41f8a8875a32bb07e1bfabb518b7d23a
PrayanshuChourasia <prayanshuchourasia440@gmail.com>
2025-02-07 11:38:33 +0530

    javascript updated and diagram add

--- SGPA/marksEntry.js
+++ SGPA/marksEntry.js
@@ -1,8 +1,17 @@
 // variables where is the data going to store for a while and then put them for the calculation for the result. 
-let Total_ThMarks = [];
-let Total_ThCr = [];
-let Total_PrMarks = [];
-let Total_PrCr = [];
+// These Variable Store the array of the marks which is taken from the input tag & the values are stored in the form of array.
+
+let All_ThMarks = [];     // These array will be filled after the function takeValue() start 
+let All_ThCr = [];        // These array will be filled after the function takeValue() start  
+let All_PrMarks = [];     // These array will be filled after the function takeValue() start 
+let All_PrCr = [];        // These array will be filled after the function takeValue() start 
+
+
+// Variables for storing the array of grade score which are derived from the All_Th / Pr Marks 
+let AllGradeScoreTh = [] ;
+let AllGradeScorePr = [] ;
+
+
 
 
 // making all the nodes of the html doc for calculation.
@@ -20,35 +29,61 @@ console.log(PrSubCr);
 
 
 
-
 function takeValue() {
-    // theory subject marks taken here in the array : Total_ThMarks.
+// theory subject marks taken here in the array : All_ThMarks.
     ThSubMarks.forEach((val) => {
-        Total_ThMarks.push(val.value);
+        All_ThMarks.push(val.value);
         console.log(val.value);
     })
-    console.log(Total_ThMarks);
+    console.log(All_ThMarks);
 
-    // Theory Subject Credit Score Taken here in the array : Total_ThCr
+// Theory Subject Credit Score Taken here in the array : All_ThCr
     ThSubCr.forEach((val)=>{
-        Total_ThCr.push(val.value);
+        All_ThCr.push(parseInt(val.value));
         console.log(val.value);
     })
-    console.log(Total_ThCr);
+    console.log(All_ThCr);
 
-    // Practical Subject Marks Taken Here in the array : 
+// Practical Subject Marks Taken Here in the array : All_PrMarks
     PrSubMarks.forEach((val)=>{
-        Total_PrMarks.push(val.value);
+        All_PrMarks.push(val.value);
         console.log(val.value);
     })
-    console.log(Total_PrMarks);
+    console.log(All_PrMarks);
 
-    // Practical Subject Credit taken here in the array : 
+// Practical Subject Credit taken here in the array :  All_PrCr
     PrSubCr.forEach((val)=>{
-        Total_PrCr.push(val.value);
+        All_PrCr.push(parseInt(val.value));
         console.log(val.value);
     })
-    console.log(Total_PrCr);
+    console.log(All_PrCr);
+
+// Variable for storing the sum of the Credit score input by the user for calculation the final result. 
+    let SumOfCrTh = All_ThCr.reduce((prev , curr )=>{   
+        return prev + curr ;                // For sum of theory credit 
+    })
+    console.log(`Sum of Theory Credit ${SumOfCrTh}`);
+    
+    let SumOfCrPr = All_PrCr.reduce((prev , curr )=>{
+        return prev + curr ;               // For the sum of practical credit 
+    })
+    console.log(`Sum of Practical Credit ${SumOfCrPr}`);
+
+
+
+// Calculation of Grade Score 
+// Steps for the calculation : 
+// 1. Find the grade point scale given by the instituation. 
+// 2. find the grade score by using the formula : grade score = grade point x credit score of that subject. 
+// 3. Calculate the sum of all the grade score and sum of all the credit score. 
+// 4. finally , calculate the SGPA using the formula : SGPA = Sum of Grade score / sum of credit score. 
+
+
+
+
+
+
 
 
 }
+
